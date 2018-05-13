4432cc10f07022c93d42e7940cb3bff96be7a9a8
beaverxsheet <beaverxsheet@gmail.com>
2018-05-13 17:25:09 +0200

    REmoving unneccessary imports folder

--- Charselect.gd
+++
@@ -1,8 +0,0 @@
-extends OptionButton
-
-func _ready():
-	#Add menu items to player select
-	add_item("Connor")
-	add_item("Niklas")
-	add_item("Anton")
-	add_item("Ben")
--- PopupPanel.gd
+++
@@ -1,9 +0,0 @@
-extends PopupPanel
-
-#Close Panel
-func _on_CloseCredits_pressed():
-	self.hide()
-
-#Show
-func _on_Credits_pressed():
-	self.popup()
