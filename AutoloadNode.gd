80f1db5f1e97abc60db78ff8e4a8e9f03083691d
beaverxsheet <beaverxsheet@gmail.com>
2018-05-13 17:25:09 +0200

    REmoving unneccessary imports folder

--- AutoloadNode.gd
+++
@@ -1,4 +0,0 @@
-extends Node
-
-#AutoloadNode allows for information to be passed on from menu scene to world
-var choose_char = null
--- TileMap.gd
+++
@@ -1,4 +0,0 @@
-extends TileMap
-
-#Set hit_ID (which is called in the protected list)
-onready var hit_ID = 1000
