77941556877a6d551de899513ae8feafe0a3bbcc
beaverxsheet <beaverxsheet@gmail.com>
2018-05-13 17:39:20 +0200

    Updating to latest state

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -2,8 +2,12 @@ extends KinematicBody2D
 
 onready var hit_ID = 2
 
+onready var myHP_Label = get_node("../HUD/Row/Player2_Cols/Player2_HP/HP_Player2")
+onready var myHP_Gauge = get_node("../HUD/Row/Player2_Cols/Player2_HP/HPGauge_P2")
+
 #Set initial HP
 var current_HP = 10
+var disp_HP = '10'
 
 func _process(delta):
 	#Upon death, hide
@@ -11,8 +15,12 @@ func _process(delta):
 		hide()
 		$CollisionShape2D.disabled = true
 	
+	if current_HP < 10:
+		disp_HP = '0' + str(current_HP)
+	
 	#Display data
-	$VBoxContainer/HP_Label.text = "HP: " + str(current_HP)
+	myHP_Label.text = "(" + disp_HP + "/10)"
+	myHP_Gauge.value = current_HP
 	
 	
 #Change HP by given value (value is set by whoever calls it)
