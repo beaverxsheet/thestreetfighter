6f36357fa7ba1159d7c9940bb8af15e553337b22
anton <nelsontrafalgar001@gmail.com>
2018-08-21 16:54:34 +0200

    Began cleaning up the "call functions" thing. This means that from now on, functions will follow the proper AID_n nomenclature.

---
+++ Scripts/AID1SneezeScene_Snezscript.gd
@@ -0,0 +1,34 @@
+extends Area2D
+
+const INTEGRAL_SPEED = 400
+
+var flyright = true
+var hit_ID = null
+
+var protected_hit_ID = [1000]
+
+func _process(delta):
+	var speed_x = 1
+	#Take var flyright from Main Scene (Player), determine direction of flight
+	if !flyright:
+		speed_x = -1
+	var speed_y = 0
+	var motion = Vector2(speed_x, speed_y)*INTEGRAL_SPEED
+	self.position = self.position + motion*delta
+	$AnimatedSprite.play("default")
+	
+	#Check if item has left screen, delete
+	if !$VisibilityNotifier2D.is_on_screen():
+		queue_free()
+
+func _on_Sneeze_body_entered(body):
+	#If the body/object hit is in the protected list, do nothing
+	if body.hit_ID in protected_hit_ID:
+		pass
+	#If hits parent object, do nothing
+	elif body.hit_ID == hit_ID:
+		pass
+	#If hits enemy/hittable object, reduce its HP
+	else:
+		queue_free()
+		body.change_HP(-5)
