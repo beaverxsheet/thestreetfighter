a34f187d42024e04bf7217d6e5ba6444ef55d424
anton <nelsontrafalgar001@gmail.com>
2018-10-07 12:13:17 +0200

    Added the full functionality of the help box. Text still needs to be added, the current dummy texts are quite annoying.

--- Scripts/PopupPanel.gd
+++ Scripts/PopupPanel.gd
@@ -1,5 +1,7 @@
 extends PopupPanel
 
+var currShow = null
+
 #Close Panel
 func _on_CloseCredits_pressed():
 	self.hide()
@@ -7,3 +9,42 @@ func _on_CloseCredits_pressed():
 #Show
 func _on_Credits_pressed():
 	self.popup()
+
+func _ready():
+	currShow = $BasicPTainer
+	currShow.show()
+
+func _on_CreditsCont_pressed():
+	currShow.hide()
+	currShow = $CreditsTainer
+	currShow.show()
+
+
+func _on_BasicP_pressed():
+	currShow.hide()
+	currShow = $BasicPTainer
+	currShow.show()
+
+
+func _on_Connor_pressed():
+	currShow.hide()
+	currShow = $ConnorTainer
+	currShow.show()
+
+
+func _on_Niklas_pressed():
+	currShow.hide()
+	currShow = $NiklasTainer
+	currShow.show()
+
+
+func _on_Anton_pressed():
+	currShow.hide()
+	currShow = $AntonTainer
+	currShow.show()
+
+
+func _on_Ben_pressed():
+	currShow.hide()
+	currShow = $BenTainer
+	currShow.show()
